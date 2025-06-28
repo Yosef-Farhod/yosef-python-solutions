af214935f711db8ba6078de40fac0025564e1c1e
Yosef <xuop87@gmail.com>
2025-06-28 11:47:15 +0300

    Edit name findet

--- 21_name_finder.py
+++ 21_name_finder.py
@@ -2,18 +2,12 @@ names = []
 
 n = int(input("Enter number names : "))
 for i in range(n) :
-    name = input(f"Enter the name {i+1} : ")
+    name = input(f"Enter the name {i+1} : ").lower()
     names.append(name)
 
 user = input("Enter sarch name : ").lower()
-sarch = True
 
-for i in names :
-    if i == user:
-        print(f"saccses find the name {i}")
-        sarch = False
-        break
-
-if sarch :
-    print(f"name {user} not find ")        
-
+if user in names :
+    print(f"Succse find name {user}")
+else :
+    print(f"Name {user} not find ")
